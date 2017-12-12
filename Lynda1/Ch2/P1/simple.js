a73d1356d707bdf07d40dbfcbca6a0138ad0fe41
Pramathesh <pramathesh.malshe@gmail.com>
2017-12-12 01:31:38 -0800

    Lynda essential

---
+++ Lynda1/Ch2/P1/simple.js
@@ -0,0 +1,9 @@
+/*alert("I love ducks");
+alert(5+8);
+alert(5.234234*83.123123/4.2123);*/
+var date = new Date();
+// alert("Today is:"+date);
+console.log("Today is:"+date);
+document.body.innerHTML="<h1>Today's date is:"+date+"</h1>";
+document.body.innerHTML="<h1>Today's date is:"+date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+"</h1>";
+console.log("Today's date is:"+date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear());
