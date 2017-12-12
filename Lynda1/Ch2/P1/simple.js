f09998e54eb100725eb624d59f3df7797cca34c6
Pramathesh <pramathesh.malshe@gmail.com>
2017-12-12 01:33:54 -0800

    Editing for correct month

--- Lynda1/Ch2/P1/simple.js
+++ Lynda1/Ch2/P1/simple.js
@@ -5,5 +5,5 @@ var date = new Date();
 // alert("Today is:"+date);
 console.log("Today is:"+date);
 document.body.innerHTML="<h1>Today's date is:"+date+"</h1>";
-document.body.innerHTML="<h1>Today's date is:"+date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+"</h1>";
-console.log("Today's date is:"+date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear());
+document.body.innerHTML="<h1>Today's date is:"+(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear()+"</h1>";
+console.log("Today's date is:"+(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear());
