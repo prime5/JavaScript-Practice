80896792c28a5e2e79d194a7d901062272046a77
Pramathesh <pramathesh.malshe@gmail.com>
2017-12-14 04:05:43 -0800

    few more simple lessons

---
+++ Lynda1/Ch2/P1/variables_data_types.js
@@ -0,0 +1,12 @@
+var a=5, b=4, sum=a+b;
+console.log(sum);
+var negInteger = -3.14159265359;
+var escQuote = "Quotes can also be \"escaped\".";
+var theSunIsWarm = true;
+var emptyInside = null;
+var justAnotherVariable;
+console.log(typeof(sum));
+console.log(typeof(escQuote));
+console.log(typeof(theSunIsWarm));
+console.log(typeof(emptyInside));
+console.log(typeof(justAnotherVariable));
